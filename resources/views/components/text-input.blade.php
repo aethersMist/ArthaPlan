1f43f05fc842c3b9ecbe30d2bcc49900367b1f31
Aurellia Nur Fitria <aur3l163@gmail.com>
2025-07-14 01:35:38 +0700

    fixing autocomplete issue

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,9 @@
-@props(['disabled' => false])
+@props(['disabled' => false, 'autocomplete' => 'on'])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'block w-full p-2.5 border-netral-light focus:border-accent focus:ring-accent rounded-lg shadow-lg']) }}>
+<input 
+    @disabled($disabled) 
+    {{ $attributes->merge([
+        'class' => 'block w-full p-2.5 border-netral-light focus:border-accent focus:ring-accent rounded-lg shadow-lg',
+        'autocomplete' => $autocomplete
+    ]) }}
+>
